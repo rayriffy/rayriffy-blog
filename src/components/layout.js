97ae023a825a0376add4c5eb721c1e8113107a9c
Phumrapee Limpianchop <git@rayriffy.com>
2018-11-13 23:45:07 +0700

    :recycle: Refactoring

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,19 +3,19 @@ import { Link } from 'gatsby'
 
 import { rhythm, scale } from '../utils/typography'
 import Footer from './footer'
-import headStyle from './head.module.css'
+import headerStyle from './head.module.css'
 import Style from './theme.module.css'
 
 class Template extends React.Component {
   render() {
     const { children } = this.props
     let header = (
-      <div className={[headStyle.header]}>
+      <div className={[headerStyle.header]}>
         <Link to={'/'}><img src="/img/LOGO-C-min.png" /></Link>
       </div>
     )
     return (
-      <div className={headStyle.cover}>
+      <div className={headerStyle.cover}>
         <div className={[Style.container]}>
           {header}
           {children}
