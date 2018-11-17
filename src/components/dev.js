ba733b9b877a51b73fe16691c80ef75f90c5fc34
Phumrapee Limpianchop <git@rayriffy.com>
2018-11-17 08:53:51 +0700

    :construction: Warning strip when deploy on development

---
+++ src/components/dev.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import devStyle from '../components/dev.module.css'
+
+class Template extends React.Component {
+  render() {
+    return (
+      <div className={devStyle.container}>
+      </div>
+    )
+  }
+}
+
+export default Template
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,25 +4,35 @@ import { graphql } from 'gatsby'
 
 import Img from 'gatsby-image'
 
+import Dev from './dev'
 import Footer from './footer'
 import layoutStyle from './layout.module.css'
 
 class Template extends React.Component {
   render() {
+    let devstrip = ''
+    if(process.env.deploy !== 'production') {
+      devstrip = (
+        <Dev />
+      )
+    }
     const { children } = this.props
     return (
-      <div className={layoutStyle.cover}>
-        <div className={[layoutStyle.container]}>
-          <div className={[layoutStyle.header]}>
-            <Link to={'/'}>
-              <Img
-                fluid={this.props.logo}
-              />
-            </Link>
+      <div>
+      {devstrip}
+        <div className={layoutStyle.cover}>
+          <div className={[layoutStyle.container]}>
+            <div className={[layoutStyle.header]}>
+              <Link to={'/'}>
+                <Img
+                  fluid={this.props.logo}
+                />
+              </Link>
+            </div>
+            {children}
           </div>
-          {children}
+          <Footer />
         </div>
-        <Footer />
       </div>
     )
   }
