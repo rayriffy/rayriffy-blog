1cc81557e7c61f712fa84a0f983fb57f41df1b0b
Phumrapee Limpianchop <git@rayriffy.com>
2018-11-20 09:24:02 +0700

    :sparkles: Query pages only /blog/

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,7 @@ exports.createPages = ({ graphql, actions }) => {
       graphql(
         `
           {
-            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000, filter: { fields: { slug: { regex: "^/blog/" } } }) {
               edges {
                 node {
                   fields {
