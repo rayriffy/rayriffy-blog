b511e2f27c799f0de71dfbe33253769932ca19ee
Phumrapee Limpianchop <git@rayriffy.com>
2018-12-24 16:44:23 +0700

    :sparkles: Navbar!!!

---
+++ src/components/navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import navbarStyle from './navbar.module.css'
+
+class Template extends React.Component {
+  render() {
+    return (
+      <nav className={navbarStyle.navbar}>
+        <ul className={navbarStyle.nav}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/category">Category</a>
+          </li>
+          <li>
+            <a
+              href="https://l.rayriffy.com/nico"
+              rel="noopener noreferrer"
+              target="_blank">
+              ♪
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Template
