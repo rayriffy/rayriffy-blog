88866337477d037fcc62e2df3c4906b071c3afd5
Phumrapee Limpianchop <git@rayriffy.com>
2018-12-24 17:12:44 +0700

    :bug: Minor fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import {graphql} from 'gatsby'
 
-import rhythm from '../utils/typography'
+import {rhythm} from '../utils/typography'
 
 import Layout from '../components/layout'
 
