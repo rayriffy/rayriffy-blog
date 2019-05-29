b9b1bc40bdf1a74dcd2e2574d396bf32bdf78a79
Phumrapee Limpianchop <git@rayriffy.com>
2019-05-29 21:34:26 +0700

    :zap: Build scripts improvement & remove code duplication

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,324 +6,229 @@ const {createFilePath} = require('gatsby-source-filesystem')
 
 const {GATSBY_ENV = 'development'} = process.env
 
-exports.createPages = ({graphql, actions}) => {
+exports.createPages = async ({graphql, actions}) => {
   const {createPage} = actions
 
   var siteUrl
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          {
-            site {
-              siteMetadata {
-                siteUrl
+  const result = await graphql(
+    `
+      {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+        allMarkdownRemark(
+          sort: {fields: [frontmatter___date], order: DESC}
+        ) {
+          edges {
+            node {
+              fields {
+                slug
               }
-            }
-            allMarkdownRemark(
-              sort: {fields: [frontmatter___date], order: DESC}
-            ) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                    subtitle
-                    status
-                    author
-                    banner {
-                      childImageSharp {
-                        fluid(maxWidth: 1000, quality: 90) {
-                          src
-                          srcSet
-                        }
-                      }
+              frontmatter {
+                title
+                subtitle
+                status
+                author
+                banner {
+                  childImageSharp {
+                    fluid(maxWidth: 1000, quality: 90) {
+                      src
+                      srcSet
                     }
                   }
                 }
               }
             }
-            allCategoriesJson {
-              edges {
-                node {
-                  key
-                  name
-                  desc
-                }
-              }
-            }
-            allAuthorsJson {
-              edges {
-                node {
-                  user
-                }
-              }
-            }
-            lifestyle: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/lifestyle/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            misc: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/misc/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            music: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/music/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            programming: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/programming/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            review: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/review/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            tutorial: allMarkdownRemark(
-              filter: {frontmatter: {category: {regex: "/tutorial/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            rayriffy: allMarkdownRemark(
-              filter: {frontmatter: {author: {regex: "/rayriffy/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
-            }
-            SiriuSStarS: allMarkdownRemark(
-              filter: {frontmatter: {author: {regex: "/SiriuSStarS/"}}}
-            ) {
-              edges {
-                node {
-                  frontmatter {
-                    status
-                  }
-                }
-              }
+          }
+        }
+        allCategoriesJson {
+          edges {
+            node {
+              key
+              name
+              desc
             }
           }
-        `,
-      )
-        .then(result => {
-          siteUrl = result.data.site.siteMetadata.siteUrl
-          var filteredResult
-          if (GATSBY_ENV === 'production' || GATSBY_ENV === 'staging') {
-            filteredResult = {
-              data: {
-                allMarkdownRemark: {edges: null},
-                allCategoriesJson: {edges: null},
-                allAuthorsJson: {edges: null},
-                lifestyle: {edges: null},
-                misc: {edges: null},
-                music: {edges: null},
-                programming: {edges: null},
-                review: {edges: null},
-                tutorial: {edges: null},
-                rayriffy: {edges: null},
-                SiriuSStarS: {edges: null},
-              },
+        }
+        allAuthorsJson {
+          edges {
+            node {
+              user
             }
-            const filterNode = [
-              'allMarkdownRemark',
-              'lifestyle',
-              'misc',
-              'music',
-              'programming',
-              'review',
-              'tutorial',
-              'rayriffy',
-              'SiriuSStarS',
-            ]
-            _.each(filterNode, node => {
-              filteredResult.data[node].edges = _.filter(
-                result.data[node].edges,
-                {node: {frontmatter: {status: 'published'}}},
-              )
-            })
-            filteredResult.data.allCategoriesJson.edges =
-              result.data.allCategoriesJson.edges
-            filteredResult.data.allAuthorsJson.edges =
-              result.data.allAuthorsJson.edges
-          } else if (GATSBY_ENV === 'development') {
-            filteredResult = result
           }
-          return filteredResult
-        })
-        .then(result => {
-          if (result.errors) {
-            console.error(result.errors)
-            reject(result.errors)
-          }
+        }
+      }
+    `,
+  )
 
-          const posts = result.data.allMarkdownRemark.edges
-          const catrgories = result.data.allCategoriesJson.edges
-          const authors = result.data.allAuthorsJson.edges
+  if (result.errors) {
+    reject(result.errors)
+  }
 
-          var filter
-          const postsPerPage = 5
-          if (GATSBY_ENV === 'production' || GATSBY_ENV === 'staging') {
-            filter = 'draft'
-          } else if (GATSBY_ENV === 'development') {
-            filter = ''
-          }
+  const postsPerPage = 5
 
-          // Create blog lists pages.
-          const numPages = Math.ceil(posts.length / postsPerPage)
+  const posts = result.data.allMarkdownRemark.edges
+  const catrgories = result.data.allCategoriesJson.edges
+  const authors = result.data.allAuthorsJson.edges
 
-          _.times(numPages, i => {
-            createPage({
-              path: i === 0 ? `/` : `/pages/${i + 1}`,
-              component: path.resolve('./src/templates/blog-list.tsx'),
-              context: {
-                limit: postsPerPage,
-                skip: i * postsPerPage,
-                status: filter,
-                numPages,
-                currentPage: i + 1,
-              },
-            })
-          })
+  let filter
+  if (GATSBY_ENV === 'production' || GATSBY_ENV === 'staging') {
+    filter = 'draft'
+  } else if (GATSBY_ENV === 'development') {
+    filter = ''
+  }
 
-          // Create blog posts pages.
-          var count = 0
-          var jsonFeed = []
-          _.each(posts, (post, index) => {
-            const previous =
-              index === posts.length - 1 ? null : posts[index + 1].node
-            const next = index === 0 ? null : posts[index - 1].node
+  // Create blog lists pages.
+  const numPages = Math.ceil(posts.length / postsPerPage)
 
-            if (count < 6) {
-              jsonFeed.push({
-                name: post.node.frontmatter.title,
-                desc: post.node.frontmatter.subtitle,
-                slug: siteUrl + post.node.fields.slug,
-                banner:
-                  siteUrl +
-                  post.node.frontmatter.banner.childImageSharp.fluid.src,
-              })
-            }
-
-            createPage({
-              path: post.node.fields.slug,
-              component: path.resolve('./src/templates/blog-post.tsx'),
-              context: {
-                author: post.node.frontmatter.author,
-                slug: post.node.fields.slug,
-                previous,
-                next,
-              },
-            })
-            count++
-          })
-
-          fs.writeFile('public/feed.json', JSON.stringify(jsonFeed), function(
-            err,
-          ) {
-            if (err) {
-              console.error(err)
-              reject(err)
-            }
-          })
-
-          // Create category pages
-          var categoryPathPrefix = '/category/'
-          _.each(catrgories, category => {
-            var totalCount = result.data[category.node.key].edges.length
-            var numCategoryPages = Math.ceil(totalCount / postsPerPage)
-            var pathPrefix = categoryPathPrefix + category.node.key
-            _.times(numCategoryPages, i => {
-              createPage({
-                path: i === 0 ? pathPrefix : `${pathPrefix}/pages/${i + 1}`,
-                component: path.resolve('./src/templates/category.tsx'),
-                context: {
-                  category: category.node.key,
-                  currentPage: i + 1,
-                  limit: postsPerPage,
-                  numPages: numCategoryPages,
-                  pathPrefix,
-                  regex: `/${category.node.key}/`,
-                  skip: i * postsPerPage,
-                  status: filter,
-                },
-              })
-            })
-          })
-
-          // Create author pages
-          var authorPathPrefix = '/author/'
-          _.each(authors, author => {
-            var totalCount = result.data[author.node.user].edges.length
-            var numAuthorPages = Math.ceil(totalCount / postsPerPage)
-            var pathPrefix = authorPathPrefix + author.node.user
-            _.times(numAuthorPages, i => {
-              createPage({
-                path: i === 0 ? pathPrefix : `${pathPrefix}/pages/${i + 1}`,
-                component: path.resolve('./src/templates/author.tsx'),
-                context: {
-                  author: author.node.user,
-                  currentPage: i + 1,
-                  limit: postsPerPage,
-                  numPages: numAuthorPages,
-                  pathPrefix,
-                  regex: `/${author.node.user}/`,
-                  skip: i * postsPerPage,
-                  status: filter,
-                },
-              })
-            })
-          })
-        }),
-    )
+  _.times(numPages, i => {
+    createPage({
+      path: i === 0 ? `/` : `/pages/${i + 1}`,
+      component: path.resolve('./src/templates/blog-list.tsx'),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        status: filter,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
   })
+
+  // Create blog posts pages.
+  var count = 0
+  var jsonFeed = []
+  _.each(posts, (post, index) => {
+    const previous =
+      index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    if (count < 6) {
+      jsonFeed.push({
+        name: post.node.frontmatter.title,
+        desc: post.node.frontmatter.subtitle,
+        slug: siteUrl + post.node.fields.slug,
+        banner:
+          siteUrl +
+          post.node.frontmatter.banner.childImageSharp.fluid.src,
+      })
+    }
+
+    createPage({
+      path: post.node.fields.slug,
+      component: path.resolve('./src/templates/blog-post.tsx'),
+      context: {
+        author: post.node.frontmatter.author,
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+    count++
+  })
+
+  fs.writeFile('public/feed.json', JSON.stringify(jsonFeed), function(
+    err,
+  ) {
+    if (err) {
+      console.error(err)
+      reject(err)
+    }
+  })
+
+  // Create category pages
+  const categoryPathPrefix = '/category/'
+
+  _.each(catrgories, async category => {
+    const categoryResult = await graphql(
+      `
+        {
+          blogs: allMarkdownRemark(
+            filter: {frontmatter: {category: {regex: "/${category.node.key}/"}}}
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  status
+                }
+              }
+            }
+          }
+        }
+      `,
+    )
+
+    var totalCount = categoryResult.data.blogs.edges.length
+
+    var categoryPages = Math.ceil(totalCount / postsPerPage)
+    var pathPrefix = categoryPathPrefix + category.node.key
+
+    _.times(categoryPages, i => {
+      createPage({
+        path: i === 0 ? pathPrefix : `${pathPrefix}/pages/${i + 1}`,
+        component: path.resolve('./src/templates/category.tsx'),
+        context: {
+          category: category.node.key,
+          currentPage: i + 1,
+          limit: postsPerPage,
+          numPages: categoryPages,
+          pathPrefix,
+          regex: `/${category.node.key}/`,
+          skip: i * postsPerPage,
+          status: filter,
+        },
+      })
+    })
+  })
+
+  // Create author pages
+  var authorPathPrefix = '/author/'
+
+  _.each(authors, async author => {
+    const authorResult = await graphql(
+      `
+        {
+          blogs: allMarkdownRemark(
+            filter: {frontmatter: {author: {regex: "/${author.node.user}/"}}}
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  status
+                }
+              }
+            }
+          }
+        }
+      `,
+    )
+
+    var totalCount = authorResult.data.blogs.edges.length
+
+    var authorPages = Math.ceil(totalCount / postsPerPage)
+    var pathPrefix = authorPathPrefix + author.node.user
+    _.times(authorPages, i => {
+      createPage({
+        path: i === 0 ? pathPrefix : `${pathPrefix}/pages/${i + 1}`,
+        component: path.resolve('./src/templates/author.tsx'),
+        context: {
+          author: author.node.user,
+          currentPage: i + 1,
+          limit: postsPerPage,
+          numPages: authorPages,
+          pathPrefix,
+          regex: `/${author.node.user}/`,
+          skip: i * postsPerPage,
+          status: filter,
+        },
+      })
+    })
+  })
+
+  return null
 }
 
 exports.onCreateNode = ({node, actions, getNode}) => {
