c335cc6970218ccd692c46f524c21d94cb4147fd
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-21 03:08:53 +0700

    :bug: Minor fix

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -26,120 +26,121 @@ interface IProps {
   },
   slug?: string
   date?: string
+  type: string
 }
 
 const SEORenderer = (data: IData, props: IProps) => {
-  console.log(props)
   const {siteMetadata} = data.site
-  const {title = siteMetadata.title, subtitle = siteMetadata.description, banner = '/default.jpg', author, slug = '', date} = props
+  const {title, subtitle = siteMetadata.description, banner = '/default.jpg', author, slug = '', date, type} = props
   const {name = siteMetadata.author, facebook = 'https://facebook.com/rayriffy', twitter = '@rayriffy'} = author
 
+  const metas = [
+    {
+      content: title ? `${siteMetadata.title} · ${title}` : `${siteMetadata.title}`,
+      name: 'name',
+    },
+    {
+      content: subtitle,
+      name: 'description',
+    },
+    {
+      content: name,
+      name: 'author',
+    },
+    {
+      content: `${siteMetadata.siteUrl}${banner}`,
+      name: 'image',
+    },
+    {
+      content: `${siteMetadata.siteUrl}${slug}`,
+      property: 'og:url',
+    },
+    {
+      content: 'article',
+      property: 'og:type',
+    },
+    {
+      content: 'th_TH',
+      property: 'og:locale',
+    },
+    {
+      content: 'en_US',
+      property: 'og:locale:alternate',
+    },
+    {
+      content: title ? `${siteMetadata.title} · ${title}` : `${siteMetadata.title}`,
+      property: 'og:title',
+    },
+    {
+      content: subtitle,
+      property: 'og:description',
+    },
+    {
+      content: siteMetadata.fbApp,
+      property: 'fb:app_id',
+    },
+    {
+      content: facebook,
+      property: 'article:author',
+    },
+    {
+      content: date,
+      property: 'article:published_time',
+    },
+    {
+      content: `${siteMetadata.siteUrl}${banner}`,
+      property: 'og:image',
+    },
+    {
+      content: `${siteMetadata.siteUrl}${banner}`,
+      property: 'og:image:secure_url',
+    },
+    {
+      content: 'banner',
+      property: 'og:image:alt',
+    },
+    {
+      content: '1500',
+      property: 'og:image:width',
+    },
+    {
+      content: '788',
+      property: 'og:image:height',
+    },
+    {
+      content: 'summary_large_image',
+      name: 'twitter:card',
+    },
+    {
+      content: twitter,
+      name: 'twitter:site',
+    },
+    {
+      content: twitter,
+      name: 'twitter:creator',
+    },
+    {
+      content: title ? `${siteMetadata.title} · ${title}` : `${siteMetadata.title}`,
+      name: 'twitter:title',
+    },
+    {
+      content: subtitle,
+      name: 'twitter:description',
+    },
+    {
+      content: `${siteMetadata.siteUrl}${banner}`,
+      name: 'twitter:image',
+    },
+    {
+      content: 'nositelinkssearchbox',
+      name: 'google',
+    },
+  ]
+
   return (
-    <Helmet
-      meta={[
-        {
-          content: `${siteMetadata.title} · ${title}`,
-          name: 'name',
-        },
-        {
-          content: subtitle,
-          name: 'description',
-        },
-        {
-          content: name,
-          name: 'author',
-        },
-        {
-          content: `${siteMetadata.siteUrl}${banner}`,
-          name: 'image',
-        },
-        {
-          content: `${siteMetadata.siteUrl}${slug}`,
-          property: 'og:url',
-        },
-        {
-          content: 'article',
-          property: 'og:type',
-        },
-        {
-          content: 'th_TH',
-          property: 'og:locale',
-        },
-        {
-          content: 'en_US',
-          property: 'og:locale:alternate',
-        },
-        {
-          content: `${siteMetadata.title} · ${title}`,
-          property: 'og:title',
-        },
-        {
-          content: subtitle,
-          property: 'og:description',
-        },
-        {
-          content: siteMetadata.fbApp,
-          property: 'fb:app_id',
-        },
-        {
-          content: facebook,
-          property: 'article:author',
-        },
-        {
-          content: date,
-          property: 'article:published_time',
-        },
-        {
-          content: `${siteMetadata.siteUrl}${banner}`,
-          property: 'og:image',
-        },
-        {
-          content: `${siteMetadata.siteUrl}${banner}`,
-          property: 'og:image:secure_url',
-        },
-        {
-          content: 'banner',
-          property: 'og:image:alt',
-        },
-        {
-          content: '1500',
-          property: 'og:image:width',
-        },
-        {
-          content: '788',
-          property: 'og:image:height',
-        },
-        {
-          content: 'summary_large_image',
-          name: 'twitter:card',
-        },
-        {
-          content: twitter,
-          name: 'twitter:site',
-        },
-        {
-          content: twitter,
-          name: 'twitter:creator',
-        },
-        {
-          content: `${siteMetadata.title} · ${title}`,
-          name: 'twitter:title',
-        },
-        {
-          content: subtitle,
-          name: 'twitter:description',
-        },
-        {
-          content: `${siteMetadata.siteUrl}${banner}`,
-          name: 'twitter:image',
-        },
-        {
-          content: 'nositelinkssearchbox',
-          name: 'google',
-        },
-      ]}>
+    <Helmet meta={metas}>
         <script type="application/ld+json" data-react-helmet="true">
-          {`
+          {type === 'article' ? `
             {
               "@context": "http://schema.org/",
               "@type" : "Article",
@@ -147,8 +148,8 @@ const SEORenderer = (data: IData, props: IProps) => {
                 "@type": "WebPage",
                 "@id": "${siteMetadata.siteUrl}"
               },
-              "name" : "${title}",
-              "headline" : "${title}",
+              "name" : "${title ? title : siteMetadata.title}",
+              "headline" : "${title ? title : siteMetadata.title}",
               "backstory" : "${subtitle}",
               "author" : {
                 "@type" : "Person",
@@ -168,7 +169,13 @@ const SEORenderer = (data: IData, props: IProps) => {
                 }
               }
             }
-          `}
+          ` : type === 'page' ? `
+            {
+              "@context": "http://schema.org/",
+              "@type" : "Website",
+              "url" : "${siteMetadata.siteUrl}"
+            }
+          ` : null}
         </script>
       </Helmet>
   )
