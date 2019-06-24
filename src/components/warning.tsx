a8000e703d0646eae4195571c19ac8941cd8859a
Phumrapee Limpianchop <git@rayriffy.com>
2019-06-24 18:26:37 +0700

    💅 Linting

--- src/components/warning.tsx
+++ src/components/warning.tsx
@@ -7,8 +7,8 @@ const Component: React.SFC = () => {
     <Card
       type={`listing`} 
       blog={{
+        subtitle: 'We are removing old ServiceWorkers from every device resulting this site to load slower than normal.',
         title: 'Removing old ServiceWorkers from every devices',
-        subtitle: 'We are removing old ServiceWorkers from every device resulting this site to load slower than normal.'
       }}
     />
   )
