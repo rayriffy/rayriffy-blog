aae6878ee8c81128e2f710cce53908ba4c7d0486
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-06 23:44:54 +0700

    :package: Try preact

--- theme/gatsby-node.js
+++ theme/gatsby-node.js
@@ -530,3 +530,20 @@ exports.createPages = async ({ graphql, actions }) => {
 
   return true
 }
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  // Requiring the server version of React-dom is hardcoded right now
+  // in the development server. So we'll just avoid loading Preact there
+  // for now.
+  if (stage !== `develop`) {
+    actions.setWebpackConfig({
+      resolve: {
+        alias: {
+          react: `preact/compat`,
+          'react-dom': `preact/compat`,
+          'react-dom/server': `preact/compat`,
+        },
+      },
+    })
+  }
+}
