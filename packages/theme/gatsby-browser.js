f13b39ffcec684a1c96dab6c66121c235c277bc7
Phumrapee Limpianchop <git@rayriffy.com>
2019-12-09 05:27:24 +0700

    :fire: Remove tries for concurrent mode

--- packages/theme/gatsby-browser.js
+++ packages/theme/gatsby-browser.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactDOM from 'ReactDOM'
 
 import App from './src/app/components'
 
@@ -9,13 +8,4 @@ export const onServiceWorkerUpdateReady = () => {
 
 export const wrapPageElement = ({ element, props }) => {
   return <App {...props}>{element}</App>
-}
-
-export const replaceHydrateFunction = () => {
-  return (element, container, callback) => {
-    ReactDOM.createRoot(container, {
-      hydrate: true,
-      hydrationOptions: { onHydrated: callback },
-    }).render(element)
-  }
 }
