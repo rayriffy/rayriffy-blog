97ad1a0b9fa8060db1781c1bccc62733cc731adf
Phumrapee Limpianchop <git@rayriffy.com>
2020-03-07 01:36:29 +0700

    chore: remove theme provider

--- packages/theme/gatsby-browser.js
+++ packages/theme/gatsby-browser.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { ThemeProvider } from 'emotion-theming'
-import { theme } from './src/utils/theme'
-
 import App from './src/app/components'
 
 export const onServiceWorkerUpdateReady = () => {
@@ -21,8 +18,4 @@ export const replaceHydrateFunction = () => {
 
 export const wrapPageElement = ({ element, props }) => {
   return <App {...props}>{element}</App>
-}
-
-export const wrapRootElement = ({ element }) => {
-  return <ThemeProvider theme={theme}>{element}</ThemeProvider>
 }
