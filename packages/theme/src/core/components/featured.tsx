f74acf278f05aa0c023df1a00f56600e06e1a219
Phumrapee Limpianchop <git@rayriffy.com>
2020-03-07 16:54:15 +0700

    style: styled featured

--- packages/theme/src/core/components/featured.tsx
+++ packages/theme/src/core/components/featured.tsx
@@ -2,68 +2,30 @@ import React from 'react'
 
 import Img from 'gatsby-image'
 
-import styled from '@emotion/styled'
-import { Box, Card, Heading, Text } from 'rebass'
-
 import TransparentLink from './transparentLink'
 
 import { IFeaturedProps } from '../@types/IFeaturedProps'
 
-interface IGatsbyImage {
-  height?: number
-}
-
-const GatsbyImage = styled(Img)<IGatsbyImage>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: -1;
-  border-radius: 0px;
-  height: auto;
-
-  @media screen and (min-width: 40em) {
-    border-radius: 6px;
-  }
-`
-
-const FeaturedCard = styled(Card)`
-  position: relative;
-  border-radius: 0px;
-  box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
-
-  @media screen and (min-width: 40em) {
-    border-radius: 6px;
-  }
-`
-
 const FeaturedComponent: React.FC<IFeaturedProps> = props => {
   const { slug, banner, featured, title, subtitle } = props
 
-  const RenderedCard = (
-    <FeaturedCard color='white' bg='rgba(0,0,0,0.3)'>
-      <GatsbyImage fluid={banner.localFile.childImageSharp.fluid} />
-      <Box
-        px={[3, 3, 4, 4]}
-        pb={[3, 3, 4, 4]}
-        style={{ position: 'absolute', left: '0', bottom: '0' }}>
-        {featured ? (
-          <Text fontSize={[14, 16, 18, 20]} mb={2}>
-            FEATURED
-          </Text>
-        ) : null}
-        <Heading fontSize={[24, 28, 32, 36]}>{title}</Heading>
-        {subtitle ? <Text fontSize={[16, 18, 20, 22]}>{subtitle}</Text> : null}
-      </Box>
-    </FeaturedCard>
+  const renderedCard = (
+    <div className='core-featured'>
+      <Img className='img' fluid={banner.localFile.childImageSharp.fluid} />
+      <div className='container'>
+        {featured ? <div className='feat'>FEATURED</div> : null}
+        <div className='title'>{title}</div>
+        {subtitle ? <div className='subtitle'>{subtitle}</div> : null}
+      </div>
+    </div>
   )
 
   return (
     <React.Fragment>
       {slug ? (
-        <TransparentLink to={`${slug}`}>{RenderedCard}</TransparentLink>
+        <TransparentLink to={`${slug}`}>{renderedCard}</TransparentLink>
       ) : (
-        RenderedCard
+        renderedCard
       )}
     </React.Fragment>
   )
