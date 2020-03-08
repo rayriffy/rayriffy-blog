d8e8526d81b26d2708437a3cc0affba8d2fd4268
Phumrapee Limpianchop <git@rayriffy.com>
2020-03-08 22:04:36 +0700

    refactor: cleanup

--- packages/theme/src/templates/404/components/index.tsx
+++ packages/theme/src/templates/404/components/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 
 import { graphql, useStaticQuery } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 import '../styles/index.styl'
 
--- packages/theme/src/templates/author/listing/components/index.tsx
+++ packages/theme/src/templates/author/listing/components/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
 
+import { Helmet } from 'react-helmet'
 import { FaFacebook, FaTwitter } from 'react-icons/fa'
 
 import Card from '../../../../core/components/card'
