a1d1e6cad6c44aedb3d8adb61b9cf54d251d97e6
Phumrapee Limpianchop <git@rayriffy.com>
2020-03-08 22:37:03 +0700

    style: styled feat/blog-listing

--- packages/theme/src/templates/blog/listing/components/index.tsx
+++ packages/theme/src/templates/blog/listing/components/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import { Box, Flex } from 'rebass'
-
 import Card from '../../../../core/components/card'
 import Featured from '../../../../core/components/featured'
 import Pagination from '../../../../core/components/pagination'
 import SEO from '../../../../core/components/seo'
+
+import '../styles/index.styl'
 
 import { IProps } from '../@types/IProps'
 
@@ -20,30 +20,26 @@ const BlogListingComponent: React.FC<IProps> = props => {
           name: 'Phumrapee Limpianchop',
           twitter: '@rayriffy',
         }}
-        type={`page`}
+        type='page'
       />
-      {page.current === 1 ? (
-        <Box mb={4}>
-          <Flex justifyContent={`center`}>
-            <Box width={[1, 18 / 24, 16 / 24, 14 / 24]}>
-              <Featured
-                title={featured.node.title}
-                subtitle={featured.node.subtitle}
-                slug={
-                  featured.node.slug.startsWith('/')
-                    ? featured.node.slug
-                    : `/${featured.node.slug}`
-                }
-                banner={featured.node.banner}
-                featured={true}
-              />
-            </Box>
-          </Flex>
-        </Box>
-      ) : null}
-      <Flex justifyContent={`center`}>
-        <Box width={[22 / 24, 22 / 24, 20 / 24, 18 / 24]}>
-          <Flex flexWrap={`wrap`} alignItems={`center`}>
+      <div className='template-blog-listing'>
+        <div className='feat'>
+          <div className='container'>
+            <Featured
+              title={featured.node.title}
+              subtitle={featured.node.subtitle}
+              slug={
+                featured.node.slug.startsWith('/')
+                  ? featured.node.slug
+                  : `/${featured.node.slug}`
+              }
+              banner={featured.node.banner}
+              featured={true}
+            />
+          </div>
+        </div>
+        <div className='list'>
+          <div className='container'>
             {blogs.map(blog => {
               const { author, title, subtitle, date, banner, slug } = blog.node
 
@@ -55,29 +51,24 @@ const BlogListingComponent: React.FC<IProps> = props => {
               }
 
               return (
-                <Box
-                  width={[1, 1, 1 / 2, 1 / 2]}
-                  p={3}
-                  key={`listing-${page.current}-${slug}`}>
+                <div className='item' key={`listing-${page.current}-${slug}`}>
                   <Card
                     author={author}
                     blog={meta}
                     slug={slug.startsWith('/') ? slug : `/${slug}`}
                     type={`listing`}
                   />
-                </Box>
+                </div>
               )
             })}
-          </Flex>
-        </Box>
-      </Flex>
-      <Box my={3}>
+          </div>
+        </div>
         <Pagination
           numPages={page.max}
           currentPage={page.current}
           pathPrefix='/'
         />
-      </Box>
+      </div>
     </React.Fragment>
   )
 }
