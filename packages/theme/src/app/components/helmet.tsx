ad247b6f7d6ecea8b1bd1db71745518f0639fac3
Phumrapee Limpianchop <git@rayriffy.com>
2020-04-28 12:23:30 +0700

    refactor: link in html instad of helmet

--- packages/theme/src/app/components/helmet.tsx
+++ packages/theme/src/app/components/helmet.tsx
@@ -20,14 +20,6 @@ const HelmetCompoent: React.FC = () => {
       htmlAttributes={{ lang: 'en' }}
       titleTemplate={`${data.site.siteMetadata.title} · %s`}
       defaultTitle={data.site.siteMetadata.title}
-      link={[
-        {
-          rel: 'stylesheet',
-          media: 'print,screen',
-          href:
-            'https://fonts.googleapis.com/css?family=Niramit|Kanit&display=swap',
-        },
-      ]}
     />
   )
 }
