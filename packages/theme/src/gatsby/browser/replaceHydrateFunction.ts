6673362c83313a4b430bd7b0cf10fa420e1a60ea
Phumrapee Limpianchop <git@rayriffy.com>
2020-05-17 19:01:42 +0700

    fix!: temporary disabled rehydrate type

--- packages/theme/src/gatsby/browser/replaceHydrateFunction.ts
+++ packages/theme/src/gatsby/browser/replaceHydrateFunction.ts
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { GatsbyBrowser } from 'gatsby'
-
-export const replaceHydrateFunction: GatsbyBrowser['replaceHydrateFunction'] = () => {
+export const replaceHydrateFunction = () => {
   return (
     element: React.DOMElement<React.DOMAttributes<Element>, Element>,
     container: Element,
