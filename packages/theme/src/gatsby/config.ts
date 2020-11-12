fd7828e3fa8c521f68b0674f4189f7748e8dec94
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-13 04:24:29 +0900

    chore: disable gatsby-remark-images-contentful

--- packages/theme/src/gatsby/config.ts
+++ packages/theme/src/gatsby/config.ts
@@ -142,18 +142,18 @@ const config: GatsbyThemeConfig = ({ contentful, seo }): GatsbyConfig => ({
             },
           },
           'gatsby-remark-embed-video',
-          {
-            resolve: `gatsby-remark-images-contentful`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 1000,
-              linkImagesToOriginal: false,
-              backgroundColor: 'rgb(60, 60, 60)',
-              withWebp: true,
-            },
-          },
+          // {
+          //   resolve: `gatsby-remark-images-contentful`,
+          //   options: {
+          //     // It's important to specify the maxWidth (in pixels) of
+          //     // the content container as this plugin uses this as the
+          //     // base for generating different widths of each image.
+          //     maxWidth: 1000,
+          //     linkImagesToOriginal: false,
+          //     backgroundColor: 'rgb(60, 60, 60)',
+          //     withWebp: true,
+          //   },
+          // },
           'gatsby-remark-responsive-iframe',
           {
             resolve: `gatsby-remark-prismjs`,
